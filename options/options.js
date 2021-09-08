0c9acc070dd97d296a15e193f3dc4401753b1e69
Crowdbotics <team@crowdbotics.com>
2021-09-08 20:19:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cer_30397Identifier",
+  url: "https://cer_30397Identifier.botics.co",
+  api: "https://cer_30397Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
